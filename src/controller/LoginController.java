45793633ac8c3c999275056486783073a8096fde
Martin Spasov <martinas95@yahoo.fr>
2016-01-23 15:44:37 -0500

    login beta

---
+++ src/controller/LoginController.java
@@ -0,0 +1,42 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private Hyperlink newAccB;
+
+    @FXML
+    private Hyperlink forgotPassB;
+
+    @FXML
+    private Button logInB;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    void forgotPassBPress(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logInPress(ActionEvent event) {
+
+    }
+
+    @FXML
+    void newAccBPress(ActionEvent event) {
+
+    }
+
+}
