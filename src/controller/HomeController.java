92165ad90b6321b481d2cc6fd18069b1124fc433
Martin Spasov <martinas95@yahoo.fr>
2016-01-23 11:42:51 -0500

    frist commit

--- src/controller/HomeController.java
+++ src/controller/HomeController.java
@@ -6,10 +6,11 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class HomeController implements Initializable {
 
-	@FXML Button myButton;
+	@FXML Label label;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
