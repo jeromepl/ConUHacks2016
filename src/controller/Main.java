063689628042f3637544200e72ec7397e260f2f4
Jerome PL <jerome-pl2@hotmail.ca>
2016-01-23 11:24:27 -0500

    Set view basic structure. Program now runs

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,10 +1,29 @@
 package controller;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+
+			Parent root = FXMLLoader.load(getClass().getResource("/view/Home.fxml"));
+
+			Scene scene = new Scene(root,400,400);
+
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
