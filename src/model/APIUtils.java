e3ab9e10f00a2e6a65ba4f73e3dee3933cde81da
Simon Bourque <simon.bourque@live.ca>
2016-01-23 11:59:31 -0500

    Add gson li

--- src/model/APIUtils.java
+++ src/model/APIUtils.java
@@ -7,7 +7,10 @@ public class APIUtils {
 	private static final String id = "MC1Psq3mFwSriro3bn6tcOu2IKyvHCcQ29Rqya4W";
 	private static final String secret = "tRAymNmwEuis04Q7m9TccbhgGqvZRUX3VstWbGO7";
 	
+	ClarifaiClient clarifai;
+	
 	public APIUtils() {
-		ClarifaiClient clarifai = new ClarifaiClient(id, secret);
+		clarifai = new ClarifaiClient(id, secret);
+		System.out.println("Success");
 	}
 }
