5877cc984b18235e0955d248c3e43836d887c2d3
Jerome PL <jerome-pl2@hotmail.ca>
2016-01-23 14:12:19 -0500

    Add sql table

--- server_side/upload.php
+++ server_side/upload.php
@@ -1,10 +1,20 @@
 <?php
+    include_once("mySQL_connection.php");
+
     if(isset($_GET['name']) && strlen($_GET['name']) > 0) {
+        
+        //Upload the file to the "files" folder
         $filename= $_GET['name'];
         $fileData=file_get_contents('php://input');
         $fhandle=fopen("files/" . $filename, 'wb');
         fwrite($fhandle, $fileData);
         fclose($fhandle);
         echo("Done uploading");
+        
+        //Add the file to the database
+        $req = $bdd->prepare('INSERT INTO files(id, name)
+								VALUES(\'\', :name)');
+        $req->execute(array('name' => $_GET['name']));
+        echo $bdd->lastInsertId();
     }
 ?>
