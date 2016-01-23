478209ea535ad623516fa0c4536b60f3e60bb266
Simon Bourque <simon.bourque@live.ca>
2016-01-23 11:05:25 -0500

    Add clarifay

--- src/model/APIUtils.java
+++ src/model/APIUtils.java
@@ -1,5 +1,13 @@
 package model;
 
+import com.clarifai.api.ClarifaiClient;
+
 public class APIUtils {
 
+	private static final String id = "MC1Psq3mFwSriro3bn6tcOu2IKyvHCcQ29Rqya4W";
+	private static final String secret = "tRAymNmwEuis04Q7m9TccbhgGqvZRUX3VstWbGO7";
+	
+	public APIUtils() {
+		ClarifaiClient clarifai = new ClarifaiClient(id, secret);
+	}
 }
